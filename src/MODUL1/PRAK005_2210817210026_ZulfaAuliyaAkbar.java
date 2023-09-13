75136360cd338092da7fe246a1c6ec8b27525c84
ZULFA AULIYA AKBAR <noro58.12@gmail.com>
2023-09-13 19:51:11 +0800

    added modul1

--- src/MODUL1/PRAK005_2210817210026_ZulfaAuliyaAkbar.java
+++ src/MODUL1/PRAK005_2210817210026_ZulfaAuliyaAkbar.java
@@ -7,9 +7,9 @@ public class PRAK005_2210817210026_ZulfaAuliyaAkbar {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Masukan Makanan Favorit: ");
+        System.out.print("Masukan Makanan Favorit: ");
         String makanan = in.next();
-        System.out.println("Masukan Hobi: ");
+        System.out.print("Masukan Hobi: ");
         String Hobi = in.next();
 
         System.out.println("Aku Suka Makan "+makanan+", dan Hobiku "+ Hobi);
